ee81bca7ef1d3c821842ac0195687d7870ec44e3
charliechen114514 <725610365@qq.com>
2024-07-15 20:18:36 +0800

    Clear Branch for demo

--- sources/CCPDFViewer/include/core/PDF_MessageBox.h
+++
@@ -1,8 +0,0 @@
-#ifndef PDF_MESSAGEBOX_H
-#define PDF_MESSAGEBOX_H
-
-#include <QMessageBox>
-
-#define LOAD_CRITI_MSGBX(Wid, ERR) QMessageBox::critical(Wid, "加载错误", ERR)
-
-#endif // PDF_MESSAGEBOX_H
